92694fd4415319f6b00ae25c7e4a8e02484e5657
Artur Carvalho <arturbelico@gmail.com>
2017-01-14 12:34:58 +0100

    add more content and original file

--- main.js
+++ main.js
@@ -108,33 +108,35 @@ window.onload = function() {
 ,{
 	"l1" : "'Isto não é brincadeira (não é fácil) para mim também, moleque.'",
 	"l2" : "'This is no picnic for me either, buster.'"
-}
-,{
-	"l1" : "",
-	"l2" : ""
-}
-,{
-	"l1" : "",
-	"l2" : ""
-}
-,{
-	"l1" : "",
-	"l2" : ""
-}
-,{
-	"l1" : "",
-	"l2" : ""
-}
-,{
-	"l1" : "",
-	"l2" : ""
-}
-,{
-	"l1" : "",
-	"l2" : ""
-}
-
-
+},
+{"l1":"Então alguns de vocês ainda estão se ajustando à volta às aulas. ","l2":"So I know some of you are still adjusting to being back at school. "},
+{"l1":"Mas eu estou aqui hoje porque eu tenho algo importante para discutir com vocês. ","l2":"But I’m here today because I have something important to discuss with you. "},
+{"l1":"Eu estou aqui porque eu quero falar com vocês sobre sua educação","l2":"I’m here because I want to talk with you about your education "},
+{"l1":"e o que se espera de vocês todos, neste novo ano letivo.","l2":"and what’s expected of all of you in this new school year. "},
+{"l1":"Eu já fiz muitos discursos sobre educação. ","l2":"Now I’ve given a lot of speeches about education. "},
+{"l1":"E eu já falei muito sobre responsabilidade.","l2":"And I’ve talked a lot about responsibility."},
+{"l1":"Eu já falei sobre a responsabilidade dos seus professores em inspirarem vocês,","l2":"I’ve talked about your teachers’ responsibility for inspiring you,"},
+{"l1":"e fazer com que aprendam.","l2":"and pushing you to learn. "},
+{"l1":"Eu já falei sobre a responsabilidade dos seus pais em garantirem que vocês fiquem no caminho certo,","l2":"I’ve talked about your parents’ responsibility for making sure you stay on track,"},
+{"l1":"e façam o dever de casa,","l2":"and get your homework done,"},
+{"l1":"e não passem o tempo todo na frente da TV ou com o Xbox.","l2":"and don’t spend every waking hour in front of the TV or with that Xbox. "},
+{"l1":"Eu já falei muito sobre a responsabilidade do governo em manter os padrões altos,","l2":"I’ve talked a lot about your government’s responsibility for setting high standards,"},
+{"l1":"apoiar os professores e diretores,","l2":"supporting teachers and principals,"},
+{"l1":"e mudar as escolas que não estão dando certo,","l2":"and turning around schools that aren’t working"},
+{"l1":"onde os alunos não estiverem tendo as oportunidades que merecem.","l2":"where students aren’t getting the opportunities they deserve."},
+{"l1":"Mas, no fim de tudo, ","l2":"But at the end of the day, "},
+{"l1":"nós podemos ter os professores mais dedicados, ","l2":"we can have the most dedicated teachers, "},
+{"l1":"os pais que mais apoiam, ","l2":"the most supportive parents, "},
+{"l1":"e as melhores escolas do mundo – ","l2":"and the best schools in the world – "},
+{"l1":"e nada disso vai importar","l2":"and none of it will matter "},
+{"l1":" a não ser que todos vocês cumpram com as suas responsabilidades.","l2":"unless all of you fulfill your responsibilities. "},
+{"l1":" A não ser que vocês estejam presentes nas escolas, ","l2":"Unless you show up to those schools; "},
+{"l1":" prestem atenção nos professores, ","l2":"pay attention to those teachers; "},
+{"l1":" ouçam seus pais, avós e outros adultos. ","l2":"listen to your parents, grandparents and other adults; "},
+{"l1":" E se esforcem da forma que é necessária para ter sucesso.","l2":"and put in the hard work it takes to succeed."},
+{"l1":"E é nisso que eu quero me concentrar hoje: ","l2":"And that’s what I want to focus on today:"},
+{"l1":"na responsabilidade que cada um de vocês têm pela sua educação. ","l2":" the responsibility each of you has for your education. "},
+{"l1":"Eu quero começar pela responsabilidade que vocês têm para consigo mesmos.","l2":" I want to start with the responsibility you have to yourself."}
 
 
 	        ]
